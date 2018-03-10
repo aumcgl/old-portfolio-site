38481d2815ba4b54120ffe317bf40538511e8d6e
Austin McGlothlin <ausmcg335ej@gmail.com>
2018-03-10 16:06:17 -0500

    Update portController.js
    JSON parsing added

--- js/controllers/portController.js
+++ js/controllers/portController.js
@@ -3,7 +3,7 @@ myApp.controller('portController', function($scope, $http) {
     $scope.test = "TEST PHRASE";
     
     $http.get('www.austin-mcg.com/projects.json').then(function(data) {
-       $scope.projects = data;
+       $scope.projects = JSON.parse(data);
    });
 
 });
