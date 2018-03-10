781ac6a4d9c2e361c28c9b3b85b271931495f4da
Austin McGlothlin <ausmcg335ej@gmail.com>
2018-03-10 16:00:44 -0500

    Update portController.js
    trying new json file path

--- js/controllers/portController.js
+++ js/controllers/portController.js
@@ -2,7 +2,7 @@ myApp.controller('portController', function($scope, $http) {
     
     $scope.test = "TEST PHRASE";
     
-    $http.get('./projects.json').then(function(data) {
+    $http.get('../../projects.json').then(function(data) {
        $scope.projects = data;
    });
 
