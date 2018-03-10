f86a961037d5aa9b15e1d24390a37c0b274789f1
Austin McGlothlin <ausmcg335ej@gmail.com>
2018-03-10 16:03:45 -0500

    Update portController.js
    second time, json path

--- js/controllers/portController.js
+++ js/controllers/portController.js
@@ -2,7 +2,7 @@ myApp.controller('portController', function($scope, $http) {
     
     $scope.test = "TEST PHRASE";
     
-    $http.get('/projects.json').then(function(data) {
+    $http.get('www.austin-mcg.com/projects.json').then(function(data) {
        $scope.projects = data;
    });
 
