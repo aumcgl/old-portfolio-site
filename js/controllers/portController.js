d8e461577d651ed2af942e68d5edbf5f15bd7c13
Austin <ausmcg335ej@gmail.com>
2018-03-10 22:15:41 -0500

    FIXED JSON project data

--- js/controllers/portController.js
+++ js/controllers/portController.js
@@ -2,9 +2,12 @@ myApp.controller('portController', function($scope, $http) {
     
     $scope.test = "TEST PHRASE";
     
-    $http.get('www.austin-mcg.com/projects.json').then(function(data) {
-       $scope.projects = JSON.parse(data);
-   });
+    $http.get('../../projects.json').then(function (response) {
+                $scope.projects= response.data;
+            },
+            function (error){
+                console.log(error);
+        });
 
 });
 
